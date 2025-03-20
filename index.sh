6560865de4865261de2f02047a9ba679a303741c
TaYaKi71751 <shantian.tw@gmail.com>
2025-03-20 15:25:04 +0900

    Initial Commit

--- index.sh
+++ index.sh
@@ -2,6 +2,7 @@
 
 chsh -s /bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 cd $HOME
 git clone https://github.com/TaYaKi71751-mac-config/etc
